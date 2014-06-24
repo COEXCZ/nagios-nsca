b5247c84df2d287b0bc68848e9758399f0f16329
Jan Češpivo <jan.cespivo@coex.cz>
2014-06-24 16:51:28 +0200

    logging handler

---
+++ nagios/log.py
@@ -0,0 +1,14 @@
+import logging
+import nagios
+
+MAP_LEVEL_NAGIOS = {
+    logging.DEBUG: nagios.OK,
+    logging.INFO: nagios.OK,
+    logging.WARNING: nagios.WARNING,
+    logging.ERROR: nagios.WARNING,
+    logging.CRITICAL: nagios.CRITICAL,
+}
+
+class NagiosHandler(logging.Handler):
+    def emit(self, record):
+        send(record.message, service_status=MAP_LEVEL_NAGIOS[record.levelno])
